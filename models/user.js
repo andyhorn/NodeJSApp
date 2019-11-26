4ef8eeb80c824bc1f73f0f5e31ea61fefbaccb65
Andy Horn <andyjhorn@gmail.com>
2019-11-26 14:53:20 -0600

    Added a basic User model with password hashing and comparison through bcrypt

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+const SALT_ROUNDS = process.env.SALT_ROUNDS || 12;
+
+module.exports = class User {
+    constructor() {
+        this.firstName = null;
+        this.lastName = null;
+        this.email = null;
+        this.password = null;
+    }
+
+    setHashedPassword(password) {
+        let salt = bcrypt.genSaltSync(SALT_ROUNDS);
+        let hash = bcrypt.hashSync(password, salt);
+        this.password = hash;
+    }
+
+    verifyPassword(password) {
+        if (password == this.password) {
+            return true;
+        }
+
+        return bcrypt.compareSync(password, this.password);
+    }
+}
